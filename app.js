b62356b055eb47b26c1668d61719cfc6884fabbf
Gin <gini2ster@gmail.com>
2018-08-04 11:04:34 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//GET Index page
+app.get('/',(req,res)=>{
+    res.send('This is working!');
+});
+
+
+
+
+
+
+const port = process.env.port || 5000;
+
+app.listen(port, ()=>{
+    console.log(`Server started on port ${port}`);
+});
+
+
