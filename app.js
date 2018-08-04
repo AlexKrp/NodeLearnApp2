6dcefe8bc5ff2025cc0a1d9f949ef43c0a112873
Gin <gini2ster@gmail.com>
2018-08-04 12:12:48 +0300

    Google auth step1

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+//PAssport config
+require('./config/passport')(passport);
+
+//Load Routes
+const auth = require('./routes/auth');
 
 const app = express();
 
@@ -11,7 +18,8 @@ app.get('/',(req,res)=>{
 
 
 
-
+//User Routers
+app.use('/auth',auth);
 
 const port = process.env.PORT || 5000;
 
