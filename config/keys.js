ffaaef5c44eb61911277d9fe6514016305cff682
AlexKrp <31034267+AlexKrp@users.noreply.github.com>
2018-09-05 11:30:56 +0300

    Delete keys.js

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb://gin:g123456@ds211592.mlab.com:11592/storybooks-dev',
-    googleClientID: '1040111053647-q0f0acc8sritdckid0e8p300g3pcccq9.apps.googleusercontent.com',
-    googleClientSecret: '8ITt39yh1OajP9pWl5cx3Jdk',
-}
